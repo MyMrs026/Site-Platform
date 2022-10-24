d8bc3266c81c1fee2faf85cad7df37429f51921c
wzj <1376839171@qq.com>
2022-10-24 18:49:37 +0800

    把AppMain.vue恢复原来的样子了，以后再也不会动了(●'◡'●)

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="app-main">
-    <transition mode="out-in" name="fade-transform">
+    <transition name="fade-transform" mode="out-in">
       <router-view :key="key" />
     </transition>
   </section>
@@ -13,18 +13,28 @@ export default {
     key() {
       return this.$route.path
     }
-  },
-  data() {
-    return {
-    }
   }
 }
 </script>
 
 <style scoped>
-
+.app-main {
+  /*50 = navbar  */
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+.fixed-header+.app-main {
+  padding-top: 50px;
+}
 </style>
 
 <style lang="scss">
-
+// fix css style bug in open el-dialog
+.el-popup-parent--hidden {
+  .fixed-header {
+    padding-right: 15px;
+  }
+}
 </style>
