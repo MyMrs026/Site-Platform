bbfd9c48aa8499f44119c7e400a685e9f5119376
wzj <1376839171@qq.com>
2022-10-24 11:29:35 +0800

    对dashboard的页面做了布局，布局情况在index.vue和AppMain.vue两个vue文件中做了详细注释。(初次做项目网页可能有点不好看QAQ)

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">xx县智慧工地监管系统</div>
+    <div class="dashboard-subtitle">日期: {{getCurrentTime()}}</div>
+    <div class="dashboard-subtitle">天气: {{weather.city}}:{{weather.wea}}</div>
   </div>
 </template>
 
@@ -9,11 +11,48 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
+  data(){
+    return{
+      weather:''
+    }
+  },
+  mounted() {
+    this.getWeather()
+  },
   computed: {
     ...mapGetters([
       'name'
     ])
-  }
+  },
+  methods: {
+    getCurrentTime() {
+      //获取当前时间并打印
+      var _this = this;
+      let yy = new Date().getFullYear();
+      let mm = new Date().getMonth() + 1;
+      let dd = new Date().getDate();
+      let hh = new Date().getHours();
+      let mf = new Date().getMinutes() < 10 ? '0' + new Date().getMinutes() : new Date().getMinutes();
+      let ss = new Date().getSeconds() < 10 ? '0' + new Date().getSeconds() : new Date().getSeconds();
+      _this.gettime = yy + '/' + mm + '/' + dd + ' ' + hh + ':' + mf + ':' + ss;
+      return _this.gettime;
+    },
+    getWeather() {
+      this.$axios
+        .get(
+          "https://v0.yiketianqi.com/api?unescape=1&version=v61&appid=23718542 &appsecret=5Z6SpskU"//appid：23718542  appsecret：5Z6SpskU
+        )
+        .then(res => {
+          // let datas = res.data.data[0];//下标为0即表示当天天气数据
+           console.log(res.data);
+           //console.log(res.data.city);
+          this.weather = res.data
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+  },
 }
 </script>
 
@@ -26,6 +65,10 @@ export default {
     font-size: 30px;
     line-height: 46px;
     text-align: center;
+  }
+  &-subtitle {
+    font-size: 20px;
+    text-align: right;
   }
 }
 </style>
